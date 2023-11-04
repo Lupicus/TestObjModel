e0cc138fe41da7f40a37e2b68b0859aea84bfcec
Lupicus <54924432+Lupicus@users.noreply.github.com>
2023-11-04 13:29:55 -0500

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TestObjModel
 Test forge ObjModel
+
+![block from 1.20.1](https://github.com/Lupicus/TestObjModel/blob/master/docs/assets/20-1.png?raw=true)
---
+++ src/main/java/com/example/testobjmodel/TestMod.java
@@ -0,0 +1,48 @@
+package com.example.testobjmodel;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(TestMod.MODID)
+public class TestMod
+{
+    public static final String MODID = "testobjmodel";
+
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+
+    public static final RegistryObject<Block> TEST_OBJ_BLOCK = BLOCKS.register("test_obj", () -> new Block(BlockBehaviour.Properties.of().noOcclusion().mapColor(MapColor.STONE)));
+    public static final RegistryObject<Item> TEST_OBJ_ITEM = ITEMS.register("test_obj", () -> new BlockItem(TEST_OBJ_BLOCK.get(), new Item.Properties()));
+    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("test_blocks", () -> CreativeModeTab.builder()
+            .withTabsBefore(CreativeModeTabs.COMBAT)
+            .title(Component.translatable("itemGroup.test_blocks"))
+            .icon(() -> Items.DEBUG_STICK.getDefaultInstance())
+            .displayItems((parameters, output) -> {
+                output.accept(TEST_OBJ_ITEM.get());
+            }).build());
+
+    public TestMod()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        BLOCKS.register(modEventBus);
+        ITEMS.register(modEventBus);
+        CREATIVE_MODE_TABS.register(modEventBus);
+    }
+}
